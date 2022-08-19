779d4514ca351c6b18e723c9eae9f0ca02967f11
hsjang <hsjang0405@gmail.com>
2022-08-20 02:27:19 +0900

    ✨ ProductController 구현 (#4)
    - displayDate를 query string으로 입력받아 조회할 때 사용
    - Pageable을 통해서 클라이언트가 page 값을 통해 상품 조회할 수 있도록 함

---
+++ src/main/java/com/example/elevenstreet/product/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.elevenstreet.product.controller;
+
+import com.example.elevenstreet.product.dto.ProductResponse;
+import com.example.elevenstreet.product.service.ProductService;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+	private final ProductService productService;
+
+	@GetMapping
+	public Page<ProductResponse> getProducts(
+		@RequestParam(name = "display_date", required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime displayDate,
+		@PageableDefault(size = 5) Pageable pageable) {
+
+		if (displayDate == null) {
+			displayDate = LocalDateTime.now();
+		}
+
+		return productService.getProducts(displayDate, pageable);
+	}
+
+}
