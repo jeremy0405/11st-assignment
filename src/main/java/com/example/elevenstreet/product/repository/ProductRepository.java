b892f209bb49fc22abbf71df4f4b3f77ed8c2dc7
hsjang <hsjang0405@gmail.com>
2022-08-20 02:21:10 +0900

    ✨ 상품 조회를 위한 ProductRepository 구현 (#4)
    - 페이지를 위한 Pageable 변수
    - 전시 기간에 맞는 상품만을 조회하기 위한 displayDate 변수

---
+++ src/main/java/com/example/elevenstreet/product/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.elevenstreet.product.repository;
+
+import com.example.elevenstreet.product.Product;
+import java.time.LocalDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	@Query(value = "select p from Product p where :displayDate between p.startsAt and p.endsAt")
+	Page<Product> findAll(Pageable pageable, LocalDateTime displayDate);
+}
