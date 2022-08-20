c698949fbc8350be5628c6136add061b57e7df40
hsjang <hsjang0405@gmail.com>
2022-08-20 15:55:22 +0900

    ✨ 상품 주문 내역 조회를 위한 OrderController, Service interface 메소드 추가 (#7)
    - query string으로 start date, end date를 받도록 함
    - 페이징 처리

--- src/main/java/com/example/elevenstreet/order/controller/OrderController.java
+++ src/main/java/com/example/elevenstreet/order/controller/OrderController.java
@@ -1,15 +1,24 @@
 package com.example.elevenstreet.order.controller;
 
 import com.example.elevenstreet.order.dto.request.OrderRequest;
+import com.example.elevenstreet.order.dto.response.OrderHistoryResponse;
 import com.example.elevenstreet.order.dto.response.OrderResponse;
 import com.example.elevenstreet.order.service.OrderService;
+import java.time.LocalDateTime;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +29,15 @@ public class OrderController {
 	private static final String USER_ID = "x-user-id";
 
 	private final OrderService orderService;
+
+	@GetMapping
+	public Page<OrderHistoryResponse> getOrderHistory(@RequestHeader(USER_ID) String userId,
+		@RequestParam("start_date") @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime startDate,
+		@RequestParam("end_date") @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime endDate,
+		@PageableDefault(size = 5) Pageable pageable) {
+
+		return orderService.getOrderHistory(userId, startDate, endDate, pageable);
+	}
 
 	@PostMapping
 	public ResponseEntity<OrderResponse> order(@RequestHeader(USER_ID) String userId,
--- src/main/java/com/example/elevenstreet/order/service/OrderService.java
+++ src/main/java/com/example/elevenstreet/order/service/OrderService.java
@@ -1,9 +1,15 @@
 package com.example.elevenstreet.order.service;
 
 import com.example.elevenstreet.order.dto.request.OrderRequest;
+import com.example.elevenstreet.order.dto.response.OrderHistoryResponse;
 import com.example.elevenstreet.order.dto.response.OrderResponse;
+import java.time.LocalDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface OrderService {
+
+	Page<OrderHistoryResponse> getOrderHistory(String userId, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 
 	OrderResponse order(String userId, OrderRequest orderRequest);
 }
