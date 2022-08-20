5991ee2838e4761f6230c6e576d4d98efa81c323
hsjang <hsjang0405@gmail.com>
2022-08-20 15:53:48 +0900

    ✨ 상품 주문 조회를 위한 OrderRepository 메소드 추가 (#7)
    - paging 처리를 위해서 fetch join이 아닌 inner join 후 spring default batch size 로 N+1 문제 해결

--- src/main/java/com/example/elevenstreet/order/repository/OrderRepository.java
+++ src/main/java/com/example/elevenstreet/order/repository/OrderRepository.java
@@ -1,8 +1,18 @@
 package com.example.elevenstreet.order.repository;
 
 import com.example.elevenstreet.order.Order;
+import java.time.LocalDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	@Query(value = "select distinct o from Order o "
+		+ "inner join o.orderProducts op "
+		+ "inner join op.product "
+		+ "where o.userId = :userId "
+		+ "and o.createdAt between :startDate and :endDate")
+	Page<Order> findByUserIdAndDateCondition(String userId, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 }
