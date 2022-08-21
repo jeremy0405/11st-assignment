556b252301760a0bded69c33130b112fab41b9cd
hsjang <hsjang0405@gmail.com>
2022-08-21 12:25:45 +0900

    ✨ Pessimistic Lock을 통한 동시성 제어 (#11)
    - 상품 레포지토리에서 Lock 어노테이션을 통해 pessimistic lock으로 동시성 제어
    - 엄청나게 몰리는 주문이 아니라면 성능이 좋지 못함

--- src/main/java/com/example/elevenstreet/product/repository/ProductRepository.java
+++ src/main/java/com/example/elevenstreet/product/repository/ProductRepository.java
@@ -2,13 +2,20 @@ package com.example.elevenstreet.product.repository;
 
 import com.example.elevenstreet.product.Product;
 import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	@Query(value = "select p from Product p where :displayDate between p.startsAt and p.endsAt")
 	Page<Product> findAll(Pageable pageable, LocalDateTime displayDate);
+
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
+	@Query("select p from Product p where p.id = :id")
+	Optional<Product> findById(Long id);
 }
